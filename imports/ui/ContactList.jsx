2b1b9cd7b8195caa86a79cc5483a2e3a3935c5b1
Anna Skuratova <smileydoodle.official@gmail.com>
2023-05-26 15:03:05 +0300

    wip/add-tailwind

--- imports/ui/ContactList.jsx
+++ imports/ui/ContactList.jsx
@@ -13,15 +13,39 @@ export const ContactList = () => {
   });
 
   return (
-    <div>
-      <h3>Contact list</h3>
+    <div className="mt-10">
+      <h3 className="text-xs font-semibold text-gray-500 uppercase tracking-wide">
+        Contact list
+      </h3>
       {isLoading ? (
-        <div>Loading</div>
+        <div className="text-sm font-medium text-gray-900">Loading...</div>
       ) : (
-        <ul>
+        <ul
+          role="list"
+          className="mt-4 border-t border-b border-gray-200 divide-y divide-gray-200"
+        >
           {contacts.map((contact) => (
-            <li key={contact.email}>
-              {contact.name} - {contact.email}
+            <li
+              key={contact.email}
+              className="py-4 flex items-center justify-between space-x-3"
+            >
+              <div className="min-w-0 flex-1 flex items-center space-x-3">
+                <div className="flex-shrink-0">
+                  <img
+                    className="h-10 w-10 rounded-full"
+                    src={contact.imageUrl}
+                    alt=""
+                  />
+                </div>
+                <div className="min-w-0 flex-1">
+                  <p className="text-sm font-medium text-gray-900 truncate">
+                    {contact.name}
+                  </p>
+                  <p className="text-sm font-medium text-gray-500 truncate">
+                    {contact.email}
+                  </p>
+                </div>
+              </div>
             </li>
           ))}
         </ul>
